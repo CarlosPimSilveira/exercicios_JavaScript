be70c0aa6a71918652e092a181ce0eb038968c4d
Carlos Pim <carlospimsilveira@gmail.com>
2024-09-24 15:47:36 -0300

    Update Codjs.js

--- Ex001/Codjs.js
+++ Ex001/Codjs.js
@@ -1,1 +1,6 @@
 window.alert('Minha primeira mensagem!'); 
+//alert so tem botão ok
+window.confirm('Está gostando de JS?')
+//confirm tem cancela e ok
+window.prompt('Qual é seu nome?')
+//tem um campo para ser digitado
