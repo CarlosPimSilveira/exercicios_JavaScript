34ba491f8ce78959c3eb66222eeb16426fa175c8
Carlos Pim <carlospimsilveira@gmail.com>
2024-10-08 07:47:19 -0300

    Estudos de JS

---
+++ Ex016/while.js
@@ -0,0 +1,21 @@
+var c = 1
+
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c recebe mais um a cada passada
+}
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
