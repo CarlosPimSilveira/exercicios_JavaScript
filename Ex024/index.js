7f767d7d8956009aef6382598aadacb7955370df
Carlos Pim <carlospimsilveira@gmail.com>
2024-10-31 07:39:56 -0300

    estudos

--- Ex024/index.js
+++ Ex024/index.js
@@ -11,4 +11,4 @@ anoNascimento = 2024 - idade
 
 console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg')
 console.log('tem', alturaEmM, 'de altura e seu IMC é de', imc)
-console.log(nome, 'nasceu em', anoNascimento, '.')
+console.log(nome, 'nasceu em', anoNascimento, '.') 
---
+++ Ex025/index.js
@@ -0,0 +1,8 @@
+const passo = 50;
+let contador = 1;
+
+contador += passo;
+contador++;
+contador++;
+
+console.log(contador); 
