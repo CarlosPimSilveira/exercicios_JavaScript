db62c286184484ae714f480e26d60ad67c34100e
Carlos Pim <carlospimsilveira@gmail.com>
2025-02-14 07:23:09 -0300

    estudos de js

---
+++ Ex050/For_Classico.js
@@ -0,0 +1,10 @@
+// agora vai de 10 em 10 
+for (let i = 0; i <= 5; i += 10) {
+    console.log(`linha ${i}`)
+}
+
+//                 0       1      2        3         4          5         6         7           8         9 
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Laranja', 'Abacaxi', 'Manga', 'Morango', 'Melancia', 'Kiwi'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i])
+}
