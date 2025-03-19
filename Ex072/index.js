494fc6e1fbfbc12eb62a9f5aeabecff6f90ce012
Carlos Pim <carlospimsilveira@gmail.com>
2025-03-19 07:28:37 -0300

    forma diferente de fazer!

---
+++ Ex072/index.js
@@ -0,0 +1,31 @@
+//Funções contrutoras (constructor functions)
+//Funções construtora -> objetos
+//funções fabrica -> objetos
+//construtora -> pessoa (new) construtora começa com maiuscula
+
+function Pessoa (nome, sobrenome) {
+    //Atributos ou métodos privados!
+    const ID = 123456
+
+    const metodoInterno = function() {
+
+    }
+
+    //Atrubutos ou metodos publicos!
+    this.nome = nome
+    this.sobrenome = sobrenome
+
+    //um metodo é uma função dentro do objeto
+    this.metodo = function () {
+        console.log('Sou um método!')
+    }
+} // Pessoa.nome =  nome
+
+const p1 = new Pessoa('Luiz', 'Otávio')
+const p2 = new Pessoa('Maria', 'Oliveira')
+
+console.log(p1.nome)
+console.log(p2.nome)
+p1.metodo();
+console.log(typeof(p1))
+
