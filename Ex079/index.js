6df60cb9de3ceee1efbc0b0b23ce1be4afb99c60
Carlos Pim <carlospimsilveira@gmail.com>
2025-04-04 09:30:33 -0300

    Update index.js

--- Ex079/index.js
+++ Ex079/index.js
@@ -49,11 +49,18 @@ console.log(p1.nomecompleto)
 function Pessoa(nome, sobrenome) {
     this.nome = nome
     this.sobrenome = sobrenome
+    Object.freeze(this) // nesse caso travamos o objeto como um todo impedindo de criar coisas ou alterar elas de forma geral.
 }
 
 // Palavra new vai criar um objeto vazio e vai pegar a palavra this 
 // {}
 const p2 = new Pessoa('Carlos', 'Miranda')
+console.log(p2.nome, p2.sobrenome)
+Object.freeze(p1) // isso "trava" o objeto nao alterando valores do mesmo
+// nesse caso nao estamos alterando a const estamos alterando o valor do valor da variavel.
+// p2 = (endereçoMemoria) -> 'valor'
+// p2 = (novoendereço) isso nao pode pq const é constante porem no caso baixo estamos mudando o valor do endereço de memoria. alterando o valor que esta no endereço de memoria. uma const nao pode ter o endereço de memoria alterando no caso abaixo ele ainda ta apontado para o mesmo endereço mas mudamos o que tava no mesmo endereço. 
+p2.nome = 'Outra coisa'
 console.log(p2.nome, p2.sobrenome)
 
 // this 
