b8f2e68b3c4cd98dc3bb1c91705326e7422e306b
Carlos Pim <carlospimsilveira@gmail.com>
2025-04-04 11:26:04 -0300

    Update index.js

--- Ex079/index.js
+++ Ex079/index.js
@@ -56,7 +56,7 @@ function Pessoa(nome, sobrenome) {
 // {}
 const p2 = new Pessoa('Carlos', 'Miranda')
 console.log(p2.nome, p2.sobrenome)
-Object.freeze(p1) // isso "trava" o objeto nao alterando valores do mesmo
+Object.freeze(p1) // isso "trava" o o bjeto nao alterando valores do mesmo
 // nesse caso nao estamos alterando a const estamos alterando o valor do valor da variavel.
 // p2 = (endereçoMemoria) -> 'valor'
 // p2 = (novoendereço) isso nao pode pq const é constante porem no caso baixo estamos mudando o valor do endereço de memoria. alterando o valor que esta no endereço de memoria. uma const nao pode ter o endereço de memoria alterando no caso abaixo ele ainda ta apontado para o mesmo endereço mas mudamos o que tava no mesmo endereço. 
