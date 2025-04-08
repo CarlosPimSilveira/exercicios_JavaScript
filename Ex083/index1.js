162a372802141beca12b190b28f46a02b23212e0
Carlos Pim <carlospimsilveira@gmail.com>
2025-04-08 12:28:31 -0300

    Update index1.js

--- Ex083/index1.js
+++ Ex083/index1.js
@@ -6,12 +6,16 @@
 
     Todos os objetos tem uma referência interna para um protótipo (__proto__) que vem da propriedade prototype da função construtora que foi usada para criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor do JS vai tentar encontrar este membro do próprio objeto e depois a cadeia do protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
 */
-// Construtora => molde (classe)
+// Construtora => molde (classe) ProtoTypes
 function Pessoa(nome, sobrenome) {
     this.nome = nome
     this.sobrenom = sobrenome
-    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome
+    //this.nomeCompleto = () => 'Original: ' + this.nome + ' ' + this.sobrenome //Esse vai ser sobrescrever o debaixo!
 } //molde para criar novos objetos!
+
+Pessoa.prototype.nomeCompleto = function() { // fazer isso melhora o desempenho pois se nos tivessemos varias pessoas ele iria criar um novo metodo para cada uma delas agora fazendo dessa forma usamos o __proto__. usando a referencia do pai dela. e todas as instancias vao usar isso melhorando o sistema
+    return this.nome + ' ' + this.sobrenome
+}
 
 // instancia
 const pessoa1 = new Pessoa('Luiz', 'O.')  // <- Pessoa = função ocnstrutora 
