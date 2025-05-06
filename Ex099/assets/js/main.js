d2781befeadfe29354d56ffc8f7d41aed9d95885
Carlos Pim <carlospimsilveira@gmail.com>
2025-05-06 07:03:07 -0300

    estudos de ajax

--- Ex099/assets/js/main.js
+++ Ex099/assets/js/main.js
@@ -2,16 +2,18 @@
 // Ajax
 
 const request = obj => {
-    const xhr = new XMLHttpRequest() //XMLHttpRequest
-    xhr.open(obj.method, obj.url, true)
-    xhr.send()
-
-    xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
-            obj.success(xhr.responseText)
-        } else {
-            obj.error(xhr.statusText)
-        }
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest() //XMLHttpRequest
+        xhr.open(obj.method, obj.url, true)
+        xhr.send()
+    
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText)
+            } else {
+                reject(xhr.statusText)
+            }
+        })
     })
 }
 
@@ -25,19 +27,20 @@ document.addEventListener('click', e => {
     }
 })
 
-function carregaPagina(el) {
+async function carregaPagina(el) {
     const href = el.getAttribute('href')
     
-    request({
+    const objConfig = {
         method: 'GET',
-        url: href,
-        success(response) {
-            carregaResultado(response)
-        },
-        error(errorText) {
-            console.log(errorText)
-        }
-    })
+        url: href
+    }
+
+    try {
+        const response = await request(objConfig)
+        carregaResultado(response)
+    } catch(e) {
+        console.log(e)
+    }
 }
 
 function carregaResultado(response) {
