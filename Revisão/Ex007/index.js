3bec2863fc1f601a149cbbc42d7c3b03de6092ba
Carlos Pim <carlospimsilveira@gmail.com>
2026-01-14 16:40:48 -0300

    Update index.js

--- Revisão/Ex007/index.js
+++ Revisão/Ex007/index.js
@@ -1,3 +1,12 @@
+let n1, n2, n3
+
 window.alert('Minha primeira mensagem!')
 window.confirm('Confirmação')
-window.prompt('Prompt')
+n1 = window.prompt('Prompt')
+
+n2 = 10
+n3 = 90
+
+window.alert(n1)
+window.alert(n2 + n3)
+
