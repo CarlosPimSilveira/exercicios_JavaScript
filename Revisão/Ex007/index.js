f82bc3ab6fc169cc95f8e896f4dbbaf50cd3358d
Carlos Pim <carlospimsilveira@gmail.com>
2026-01-15 07:35:09 -0300

    Update index.js

--- Revisão/Ex007/index.js
+++ Revisão/Ex007/index.js
@@ -10,3 +10,41 @@ n3 = 90
 window.alert(n1)
 window.alert(n2 + n3)
 
+const num1 = Number(prompt("Digite o primeiro número:"));
+
+const operador = prompt("Digite o operador (+, -, *, /):");
+
+const num2 = Number(prompt("Digite o segundo número:"));
+
+let resultado;
+
+switch (operador) {
+    case "+":
+        resultado = num1 + num2;
+        break;
+
+    case "-":
+        resultado = num1 - num2;
+        break;
+
+    case "*":
+        resultado = num1 * num2;
+        break;
+
+    case "/":
+        if (num2 === 0) {
+            alert("Não é possível dividir por zero");
+            resultado = null;
+        } else {
+            resultado = num1 / num2;
+        }
+        break;
+
+    default:
+        alert("Operador inválido");
+        resultado = null;
+}
+
+if (resultado !== null) {
+    alert("Resultado: " + resultado);
+}
