5f397b3d42d0ca7ea65c5dfd50d010d898284cdd
Carlos Pim <carlospimsilveira@gmail.com>
2026-01-20 19:54:47 -0300

    while

---
+++ Revisão/Ex013/While.js
@@ -0,0 +1,13 @@
+var a = 1
+while (a <= 6) {
+    console.log(`Passo: ${a}`)
+    a++
+}
+
+console.log('Do while abaixo')
+
+var b = 1
+do {
+    console.log(`Passo: ${b}`) 
+    b++
+} while (b <= 6)
