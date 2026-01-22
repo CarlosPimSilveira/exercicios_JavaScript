c643a2a4b1ab8e8bc50eecdb07ec05dcf5b2b95b
Carlos Pim <carlospimsilveira@gmail.com>
2026-01-22 06:47:54 -0300

    estudo

--- Revisão/Ex014/Ex002/script.js
+++ Revisão/Ex014/Ex002/script.js
@@ -1,1 +1,19 @@
-alert("ola")
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option') //crio o elemento option
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //chamando cada um de tab 1, 2, 3, 4... etc para saber qual foi selecionado
+            tab.appendChild(item)  //elemento filho
+            c++
+        }
+    }
+}
