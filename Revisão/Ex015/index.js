ea19b7af82c8ee3d06f982a4fc3c6a21ddeed897
Carlos Pim <carlospimsilveira@gmail.com>
2026-01-23 07:03:08 -0300

    revisão

---
+++ Revisão/Ex015/index.js
@@ -0,0 +1,19 @@
+let a = [10, 400, 1051] // Array - Vetor
+console.log(a[1])
+console.log(a[2])
+console.log(a[0])
+
+let num = [5, 8, 2, 9, 3]
+console.log(`Nosso vetor é o ${num}`)
+num[5] = 6
+console.log(`Nosso vetor é o ${num}`)
+num.push(7) //aqui nao precisamos mostrar o ultimo lugar ele acha sozinho
+console.log(`Nosso vetor é o ${num}`)
+console.log(num.length) //Diz o tamanho do nosso array 5, 8, 2, 9, 3, 6, 7 = 0123456 = 7
+num.sort() // Organiza em ordem crescente menor para maior
+console.log(`Nosso vetor é o ${num}`) 
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+for(n = 0; n < num.length; n++) {
+    console.log(num[n])
+}
