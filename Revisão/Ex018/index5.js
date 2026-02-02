009fef22526f289654841a5985e1ec8f1fd36db7
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-02 17:15:44 -0300

    Create index5.js

---
+++ Revisão/Ex018/index5.js
@@ -0,0 +1,9 @@
+let num1 = 0.7
+let num2 = 0.1
+
+let temp = num1 + '5'
+console.log(num1 + num2)
+console.log(num1.toString() + num2)
+console.log(num1.toFixed(2))
+console.log(Number.isInteger(num1))
+console.log(Number.isNaN(temp))
