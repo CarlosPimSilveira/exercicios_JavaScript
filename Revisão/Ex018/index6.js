07b2f602f8ea03f95740d544b5f4b8687c78fe39
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-03 07:23:18 -0300

    Create index6.js

---
+++ Revisão/Ex018/index6.js
@@ -0,0 +1,15 @@
+let num1 = 9.54578
+//let num2 = Math.floor(num1) // faz arredondamento para baixo
+//let num2 = Math.ceil(num1) // faz arredondamento para cima
+//let num2 = Math.round(num1) // faz arredondamento de forma automatica
+console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)) // pega o maior numero
+console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)) // pega o menor numero
+console.log(Math.random()) // gera numero aleatorio entre 0 e 1
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5) // agora meu numero aleatorio fica entre 10 e 5 - agora fiz para arredondar o numero aleatorio
+console.log(aleatorio)
+console.log(Math.pow(2, 10)) // elevado
+
+let num2 = 9
+console.log(num2 ** (1/2)) // raiz quadrada
+console.log(num2 ** 0.5) // raiz quadrada
