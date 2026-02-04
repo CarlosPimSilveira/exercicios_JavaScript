63a447d9b17443cb637455e13333f7a7025fc51a
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-04 07:03:37 -0300

    estudos

---
+++ Revisão/Ex019/index.js
@@ -0,0 +1,40 @@
+let num = Number(window.prompt('Disite um número: '))
+
+let h1Num = window.document.getElementById('h1Num')
+h1Num.innerText += ' ' + num
+
+// Raiz quadrada
+var p1 = document.createElement('p')
+p1.setAttribute('id', 'p1')
+p1.innerText = 'Raiz quadrada: ' + num ** 0.5
+document.body.appendChild(p1)
+
+// Inteiro:
+var p2 = document.createElement('p')
+p2.setAttribute('id', 'p2')
+p2.innerText = `${num} é inteiro? ${Number.isInteger(num)}`
+document.body.appendChild(p2)
+
+// NaN
+var p3 = document.createElement('p')
+p3.setAttribute('id', 'p3')
+p3.innerText = `é NaN: ${Number.isNaN(num)}`
+document.body.appendChild(p3)
+
+// Arredondando para baixo
+var p4 = document.createElement('p')
+p4.setAttribute('id', 'p4')
+p4.innerText = `Arredondando para baixo: ${Math.floor(num)}`
+document.body.appendChild(p4)
+
+// Arredondando para cima
+var p5 = document.createElement('p')
+p5.setAttribute('id', 'p5')
+p5.innerText = `Arredondando para cima: ${Math.ceil(num)}`
+document.body.appendChild(p5)
+
+// duas casas decimais
+var p6 = document.createElement('p')
+p6.setAttribute('id', 'p6')
+p6.innerText = `Com duas casas decimais: ${num.toFixed(2)}`
+document.body.appendChild(p6)
