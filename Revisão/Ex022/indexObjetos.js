55481526d4151c25f4b28b4673c8511a49a11650
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-10 07:00:59 -0300

    Update indexObjetos.js

--- Revisão/Ex022/indexObjetos.js
+++ Revisão/Ex022/indexObjetos.js
@@ -6,17 +6,35 @@ console.log(array)
 const pessoa1 = {
     nome: 'Luiz',
     sobrenome: 'Miranda',
-    idade: 25
+    idade: 25,
+
+    fala() {
+        console.log(`${this.nome} ${this.sobrenome} está falando oi..., a minha idade é ${this.idade}`)
+    },
+    
+    incrementaIdade() {
+        this.idade++
+    }
 }
 console.log(pessoa1.nome)
+pessoa1.fala()
+pessoa1.incrementaIdade()//26
+pessoa1.incrementaIdade()//27
+pessoa1.incrementaIdade()//28
+pessoa1.fala()
+
 
 // Fabrica de objeto
 function criaPessoa(nome, sobrenome, idade) {
     return {
-        nome: nome,
-        sobrenome: sobrenome,
-        idade: idade
+        nome, sobrenome, idade
     }
 }
 const pessoa2 = criaPessoa('Carlos', 'Pim', 27)
+const pessoa3 = criaPessoa('Maria', 'Oliveira', 32)
+const pessoa4 = criaPessoa('João', 'Moreira', 55)
+const pessoa5 = criaPessoa('Jean', 'Otávio', 39)
 console.log(pessoa2)
+console.log(pessoa3)
+console.log(pessoa4)
+
