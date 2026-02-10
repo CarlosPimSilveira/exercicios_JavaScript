be7fa8fecf1f33c0d1188bb4c1d436ddd406e5f5
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-10 06:26:21 -0300

    Update index.js

--- Revisão/Ex022/index.js
+++ Revisão/Ex022/index.js
@@ -4,3 +4,10 @@ function saudacao(nome) {
 
 const variavel = saudacao('luiz')
 console.log(variavel)
+
+function soma(n1, n2) {
+    const resultado = n1 + n2
+    return resultado
+}
+
+console.log(soma(10, 5))
