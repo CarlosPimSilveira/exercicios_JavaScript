c7a2339f175d339cbcac7e5e7047455bc05ec2a4
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-10 06:35:02 -0300

    Update index.js

--- Revisão/Ex022/index.js
+++ Revisão/Ex022/index.js
@@ -1,13 +1,19 @@
 function saudacao(nome) {
     return `Bom dia ${nome}`
 } 
-
 const variavel = saudacao('luiz')
 console.log(variavel)
 
-function soma(n1, n2) {
+function soma(n1 = 0, n2 = 0) { //caso não seja enviado valor para o parametro
     const resultado = n1 + n2
     return resultado
 }
-
 console.log(soma(10, 5))
+
+function protegido() {
+    const prot = 'Prot é uma variavel que esta protegido e visivel dentro apenas da function'
+    return prot
+    console.log('E tudo abaixo do return não é execultado')
+}
+console.log(protegido())
+
