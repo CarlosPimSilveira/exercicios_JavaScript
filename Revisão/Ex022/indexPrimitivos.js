c4c40e9bb40b0890f679cbdabd123a48a0db662f
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-11 14:12:27 -0300

    Update indexPrimitivos.js

--- Revisão/Ex022/indexPrimitivos.js
+++ Revisão/Ex022/indexPrimitivos.js
@@ -18,4 +18,23 @@ let D = C //Diferente de antes isso não é uma copia eles estao apontando para 
 console.log(C, D)
 C.push(4)
 D.pop() //ambos C e D vao ser afetados!
-console.log(C, D)
+console.log(C, D) 
+
+// Para realizar a copia
+let N1 = [1, 2, 3, 4]
+let N2 = [...N1] // Nesse caso estamos realmente copiando o valor e não apontando o caminho!
+// Agora ambos trabalham de forma individual!
+N2.push(5)
+N1.pop() 
+console.log(N1, N2)
+
+// Agora com objetos é a mesma coisa
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+}
+const pessoa1 = {...pessoa} //Dessa forma é uma copia e nao aponta para o mesmo local!
+
+pessoa.nome = 'Carlos'
+console.log(pessoa, pessoa1)
