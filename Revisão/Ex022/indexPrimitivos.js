2644b58b2f5fc971776c736103372e22c9d6638b
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-11 13:24:16 -0300

    Update indexPrimitivos.js

--- Revisão/Ex022/indexPrimitivos.js
+++ Revisão/Ex022/indexPrimitivos.js
@@ -1,5 +1,6 @@
 /*
     Primitivos - String, number, boolean, undefined, null (bigint, symbol) - valor
+    Referencia (Mutavel) - array, object, function
 */
 let nome = 'Luiz'
 nome = 'Otavio'
@@ -7,5 +8,14 @@ console.log(nome[0])
 
 let a = 'A'
 let b = a //copia
-
 console.log(a, b)
+a = 'Outra coisa'
+console.log(a, b)
+
+// Ambas vão ser afetadas diferente de antes
+let C = [1, 2, 3]
+let D = C //Diferente de antes isso não é uma copia eles estao apontando para o mesmo lugar na memoria!
+console.log(C, D)
+C.push(4)
+D.pop() //ambos C e D vao ser afetados!
+console.log(C, D)
