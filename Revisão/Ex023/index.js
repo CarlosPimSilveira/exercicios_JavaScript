1beb5a41ce72c7f1d99854d38afc13e6844d50fb
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-24 20:21:57 -0300

    Update index.js

--- Revisão/Ex023/index.js
+++ Revisão/Ex023/index.js
@@ -1,8 +1,11 @@
 function Enviar() {
     let txtNome = window.document.getElementById('NomeTXT').value
+    let txtSobrenome = window.document.getElementById('SobrenomeTXT').value
+    let txtPeso = window.document.getElementById('PesoTXT').value
+    let txtAltura = window.document
 
     var p = document.createElement('p')
     p.setAttribute("id", "p")
     document.body.appendChild(p)
-    p.innerText = txtNome;
+    p.innerText = txtNome + ' ' + txtSobrenome + ' ' + txtPeso;
 }
