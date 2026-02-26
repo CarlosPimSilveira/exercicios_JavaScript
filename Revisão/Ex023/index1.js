e08c4630688f11551c340c782e4a57b1ed7b7b9a
Carlos Pim <carlospimsilveira@gmail.com>
2026-02-26 19:04:02 -0300

    Update index1.js

--- Revisão/Ex023/index1.js
+++ Revisão/Ex023/index1.js
@@ -1,6 +1,6 @@
 /*
     Operadores Lógicos
-    && -> AND -> E - Todas as expressões precisam ser verdadeiras para retornar true
-    || -> OR -> OU
-    ! -> NOT -> NÃO
+    && -> AND -> E  - Todas as expressões precisam ser verdadeiras para retornar true
+    || -> OR -> OU  - Um valor precisa obrigatoriamente ser verdadeiro
+    ! -> NOT -> NÃO - Passa a negar ou seja !(false) entao isso retorna true
 */
