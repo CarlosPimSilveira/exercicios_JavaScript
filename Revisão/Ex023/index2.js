f5dd0eff4486edd4bd4a007c6d950ad6c3e75d4d
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-04 06:35:20 -0300

    Update index2.js

--- Revisão/Ex023/index2.js
+++ Revisão/Ex023/index2.js
@@ -4,6 +4,8 @@ if (hora < 0 && hora <= 11) {
     console.log('Bom dia!')
 } else if (hora >= 12 && hora <= 17) {
     console.log('Boa tarde!')
-} else if (hora < 23) {
+} else if (hora >= 18 && hora <= 23) {
     console.log('Boa noite!')
+} else {
+    console.log('Horario invalido!')
 }
