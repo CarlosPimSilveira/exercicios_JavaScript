a5f1fa4d38218307fcfb7d266b3842785ba842ee
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-05 06:56:34 -0300

    estudos

--- Revisão/Ex024/assets/js/main.js
+++ Revisão/Ex024/assets/js/main.js
@@ -3,17 +3,20 @@ const form = document.querySelector('#formulario')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    console.log('Evento parado!')
-    setResultado()
+    
 })
 
 function setResultado(msg) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = ''
 
-    const p = document.createElement('p') // Cria uma tag p
-    p.classList.add('paragrafo-resultado')
-    p.innerHTML = 'Qualquer coisa'
+    const p = criaP()
+
     // Adicione esse filho ao resultado
-    resultado.appendChild(p)
+    // resultado.appendChild(p)
+}
+
+function criaP() {
+    const p = document.createElement('p') // Cria uma tag p
+    return p
 }
