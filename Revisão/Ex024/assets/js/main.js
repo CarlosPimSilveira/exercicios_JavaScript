588ece1f6742fc8859a4df2a88f6961e5ab83580
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-05 06:44:33 -0300

    Update main.js

--- Revisão/Ex024/assets/js/main.js
+++ Revisão/Ex024/assets/js/main.js
@@ -1,3 +1,7 @@
 // Capturar evento de submit do formulario!
 const form = document.querySelector('#formulario')
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log('Evento parado!')
+})
