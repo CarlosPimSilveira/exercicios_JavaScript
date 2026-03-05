6063b9ac9a7859e517fa88d3c13c38c8f298f435
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-05 06:59:16 -0300

    Update main.js

--- Revisão/Ex024/assets/js/main.js
+++ Revisão/Ex024/assets/js/main.js
@@ -3,7 +3,13 @@ const form = document.querySelector('#formulario')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    
+    const inputPeso = e.target.querySelector('#peso')
+    const inputAltura = e.target.querySelector('#altura')
+
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    console.log(peso, altura)
 })
 
 function setResultado(msg) {
