40e21e8a4c3f08aa6c944df94fdcba4122c46263
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-06 16:09:25 -0300

    estudos

--- Revisão/Ex025/index.js
+++ Revisão/Ex025/index.js
@@ -3,6 +3,7 @@
 // condição ? 'Valor para verdadeiro' : 'Valor para falso'
 const pontuacaoUsuario = 999
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal'
+
 console.log(nivelUsuario)
 
 /*
