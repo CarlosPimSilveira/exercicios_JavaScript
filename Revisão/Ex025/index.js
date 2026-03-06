92662d1c637b4ede77dfed182ce8b3b8f986fb59
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-06 06:32:17 -0300

    Update index.js

--- Revisão/Ex025/index.js
+++ Revisão/Ex025/index.js
@@ -3,8 +3,12 @@
 
 const pontuacaoUsuario = 999
 
+pontuacaoUsuario >= 1000 ? console.log('Usuário Vip') : console.log('Usuário normal')
+
+/*
 if (pontuacaoUsuario >= 1000) {
     console.log('Usuário Vip')
 } else {
     console.log('Usuário normal')
 }
+*/
