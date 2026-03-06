cd81e13ef549aaf009e4599b2aab28a923a90216
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-06 06:36:28 -0300

    Update index.js

--- Revisão/Ex025/index.js
+++ Revisão/Ex025/index.js
@@ -1,6 +1,6 @@
 // Operadores ternarios
 // ? :
-
+// condição ? 'Valor para verdadeiro' : 'Valor para falso'
 const pontuacaoUsuario = 999
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal'
 console.log(nivelUsuario)
