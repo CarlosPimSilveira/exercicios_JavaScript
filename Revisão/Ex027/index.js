7f4a96f1996dcde4983f7c8bad4bff6e296e65f8
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-16 16:52:52 -0300

    Update index.js

--- Revisão/Ex027/index.js
+++ Revisão/Ex027/index.js
@@ -2,9 +2,28 @@ const data = new Date()
 const diaSemana = data.getDay()
 let diaSemanatexto
 
-if (diaSemana === 1) {
-    diaSemanatexto = 'Segunda'
+switch (diaSemana) {
+    case 0: 
+        diaSemanatexto = 'Domingo'
+        break   
+    case 1: 
+        diaSemanatexto = 'Segunda'
+        break
+    case 2: 
+        diaSemanatexto = 'Terça'
+        break
+    case 3: 
+        diaSemanatexto = 'Quarta'
+        break
+    case 4: 
+        diaSemanatexto = 'Quinta'
+        break
+    case 5: 
+        diaSemanatexto = 'Sexta'
+        break
+    case 6: 
+        diaSemanatexto = 'Sabado'
+        break
 }
-
 
 console.log(diaSemanatexto)
