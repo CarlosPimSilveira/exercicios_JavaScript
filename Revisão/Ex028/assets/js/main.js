806fcc5b7e45c429e13e97bed3ca2cfa2fa028ae
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-17 06:55:36 -0300

    ex

--- Revisão/Ex028/assets/js/main.js
+++ Revisão/Ex028/assets/js/main.js
@@ -26,31 +26,45 @@ function DiasemanaNum(diaSemana) {
     }
 }
 
-function mesNum() {
+function MesNum(mes) {
     switch (mes) {
-        case 0: return 'Janeiro'
-        case 1: return 'Fevereiro'
-        case 2: return 'Março'
-        case 3: return 'Abril'
-        case 4: return 'Maio'
-        case 5: return 'Junho'
-        case 6: return 'Julho'
-        case 7: return 'Agosto'
-        case 8: return 'Setembro'
-        case 9: return 'Outubro'
-        case 10: return 'Novembro'
-        case 11: return 'Dezembro'
-        default: return ''
+        case 0: 
+            return 'Janeiro'
+        case 1: 
+            return 'Fevereiro'
+        case 2: 
+            return 'Março'
+        case 3: 
+            return 'Abril'
+        case 4: 
+            return 'Maio'
+        case 5: 
+            return 'Junho'
+        case 6: 
+            return 'Julho'
+        case 7: 
+            return 'Agosto'
+        case 8: 
+            return 'Setembro'
+        case 9: 
+            return 'Outubro'
+        case 10: 
+            return 'Novembro'
+        case 11: 
+            return 'Dezembro'
+        default: 
+            return ''
     }
 }
 
 function resultado() {
     const resultado = document.querySelector('.container')
     const p = criaP()
+    p.classList.add('paragrafo-resultado')
     const diaSemana = DiasemanaNum(data.getDay())
     const mes = MesNum(data.getMonth())
 
-    p.innerHTML = `${diaSemana}, ${data.getDate()} de ${}`
+    p.innerHTML = `${diaSemana}, ${data.getDate()} de ${mes} de ${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}`
     resultado.appendChild(p)
 }
 
