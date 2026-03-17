507a0c41976985bfd416e9ffdadca1b68a3d2f99
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-17 06:47:02 -0300

    Update main.js

--- Revisão/Ex028/assets/js/main.js
+++ Revisão/Ex028/assets/js/main.js
@@ -5,9 +5,33 @@ function criaP() {
     return p
 }
 
+function DiasemanaNum() {
+switch (diaSemana) {
+    case 0: 
+        return diaSemanatexto = 'Domingo'   
+    case 1: 
+        return diaSemanatexto = 'Segunda'
+    case 2: 
+        return diaSemanatexto = 'Terça'
+    case 3: 
+        return diaSemanatexto = 'Quarta'
+    case 4: 
+        return diaSemanatexto = 'Quinta'
+    case 5: 
+        return diaSemanatexto = 'Sexta'
+    case 6: 
+        return diaSemanatexto = 'Sabado'
+    default:
+        diaSemana = ''
+}
+}
+
 function resultado() {
+    const resultado = document.querySelector('.container')
     const p = criaP()
-    p.innerHTML = data.getDate()
+    const diaSemana = DiasemanaNum(data.getDay())
+
+    p.innerHTML = `${data.getDate()}/${data.getMonth()} ${diaSemana}`
     resultado.appendChild(p)
 }
 
