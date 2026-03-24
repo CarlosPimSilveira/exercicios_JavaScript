4f66ebb3ae77bf83fe0794fa6112d6a143dad6df
Carlos Pim <carlospimsilveira@gmail.com>
2026-03-24 14:06:24 -0300

    Update index1.js

--- Revisão/Ex025/index1.js
+++ Revisão/Ex025/index1.js
@@ -1,3 +1,11 @@
-function dark {
-    const fundo = 
+function darkMode() {
+    const fundo = document.body;
+
+    if (document.getElementById('dark-btn').checked) {
+        fundo.style.backgroundColor = '#121212';
+        fundo.style.color = '#ffffff';
+    } else {
+        fundo.style.backgroundColor = '#ffffff';
+        fundo.style.color = '#000000';
+    }
 }
