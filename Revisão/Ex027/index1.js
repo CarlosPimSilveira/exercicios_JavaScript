90b8c1c00cd15c5b71004436422d497636eed051
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-01 13:42:35 -0300

    Update index1.js

--- Revisão/Ex027/index1.js
+++ Revisão/Ex027/index1.js
@@ -4,6 +4,8 @@ let nome = 'Luiz'
 var nome2 = 'Luiz'
 
 if (verdadeira) {
+    // trocamos o valor de nome dentro apenas do if ou seja fora ele ainda é luiz
+    // Let tem escopo de bloco {}
     let nome = 'Carlos'
     console.log(nome, nome2)
 }
