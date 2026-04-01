b45c679705fc92fc5f661018c31882cf263c464a
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-01 13:43:44 -0300

    Update index1.js

--- Revisão/Ex027/index1.js
+++ Revisão/Ex027/index1.js
@@ -6,8 +6,13 @@ var nome2 = 'Luiz'
 if (verdadeira) {
     // trocamos o valor de nome dentro apenas do if ou seja fora ele ainda é luiz
     // Let tem escopo de bloco {}
-    let nome = 'Carlos'
+    // var so tem escopo de função
+    let nome = 'Carlos' // criando uma outra variavel nome com outro valor
     console.log(nome, nome2)
+
+    if (verdadeira) {
+        console.log('OK')
+    }
 }
 
 console.log(nome, nome2)
