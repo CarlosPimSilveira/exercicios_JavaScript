b8f63a4b026604b85c18870aa3040129be4416df
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-02 07:07:38 -0300

    Update index2.js

--- Revisão/Ex027/index2.js
+++ Revisão/Ex027/index2.js
@@ -12,6 +12,8 @@ console.log(a, b, c)
 //               0    1    2    3    4    5    6    7    8 
 const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900]
 const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros
+const [um, , tres, , cinco, , sete] = numeros
 
 console.log(primeiroNumero, segundoNumero, terceiroNumero)
-console.log(resto) // rest
+console.log(resto) // rest ou spread
+console.log(um, tres, cinco, sete)
