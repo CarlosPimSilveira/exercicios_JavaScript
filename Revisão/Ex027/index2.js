e227e80925974df0574436d20c29bad2248daffe
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-02 07:03:51 -0300

    Update index2.js

--- Revisão/Ex027/index2.js
+++ Revisão/Ex027/index2.js
@@ -8,3 +8,10 @@ const letras = [b, c, a]; // invertemos
 [a, b, c] = letras; 
 
 console.log(a, b, c)
+
+//               0    1    2    3    4    5    6    7    8 
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros
+
+console.log(primeiroNumero, segundoNumero, terceiroNumero)
+console.log(resto) // rest
