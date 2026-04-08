20726609a469a56760fe8d435869b953d49de916
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-08 15:47:42 -0300

    Update index6.js

--- Revisão/Ex027/index6.js
+++ Revisão/Ex027/index6.js
@@ -1,9 +1,10 @@
 const frutas = ['Pera', 'Maça', 'Uva'];
+//                 0       1      2
 
 for (let i = 0; i < frutas.length; i++) {
     //console.log(frutas[i])
 }
 
-for (let i in frutas) {
-    console.log(frutas[i])
+for (let indice in frutas) {
+    console.log(frutas[indice])
 }
