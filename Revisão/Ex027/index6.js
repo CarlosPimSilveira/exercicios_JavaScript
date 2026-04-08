3fb5e0207234400ad55c219ff0323857283ee08d
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-08 15:49:03 -0300

    Update index6.js

--- Revisão/Ex027/index6.js
+++ Revisão/Ex027/index6.js
@@ -5,6 +5,17 @@ for (let i = 0; i < frutas.length; i++) {
     //console.log(frutas[i])
 }
 
+// For in -> le os indices ou chaves do objetos!
 for (let indice in frutas) {
     console.log(frutas[indice])
+}
+
+const pessoa = {
+    nome: 'Carlos',
+    sobrenome: 'Pim',
+    idade: 27
+}
+
+for (let i in pessoa) {
+    console.log(pessoa[i])
 }
