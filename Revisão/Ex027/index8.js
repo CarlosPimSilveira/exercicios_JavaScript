d0e45e83e284f632df6b5d04bca3f825e49f5d2c
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-14 13:53:36 -0300

    Update index8.js

--- Revisão/Ex027/index8.js
+++ Revisão/Ex027/index8.js
@@ -7,4 +7,6 @@ for (let chave in pessoa) {
     console.log(chave, pessoa[chave])
 }
 
-
+// For clássico - geralmente com iteráveis (array ou strings)
+// For in - retorna o índice ou chavez (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
