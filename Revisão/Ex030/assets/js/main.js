5b127f85d1235259907def2f07fc1f5244437478
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-15 06:26:22 -0300

    Update main.js

--- Revisão/Ex030/assets/js/main.js
+++ Revisão/Ex030/assets/js/main.js
@@ -1,8 +1,12 @@
 const paragrafos = document.querySelector('.paragrafos')
 const ps = paragrafos.querySelectorAll('p')
+const estilosBody = getComputedStyle(document.body) 
+// Pega tudo que tem de estilo no body
+const backgoundColorBody = estilosBody.backgroundColor
+
 
 for (let p of ps) {
-    
+    p.style.backgroundColor = backgoundColorBody
 }
 
 
