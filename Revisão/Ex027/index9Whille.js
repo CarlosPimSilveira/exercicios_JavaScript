0732196099f5968cae6a254cbf8cb67055e5e865
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-15 06:46:54 -0300

    Update index9Whille.js

--- Revisão/Ex027/index9Whille.js
+++ Revisão/Ex027/index9Whille.js
@@ -15,3 +15,23 @@ while (controle <= 10) {
 }
 
 console.log('Segue a vida...')
+console.log('####')
+
+// Gera numero aleatorio
+function random(min, max) {
+    const r = Math.random() * (max - min) + min
+    return Math.floor(r)
+    // Pegando "inteiro"
+}
+
+const min = 1
+const max = 50
+let rand = random(min, max)
+
+// Enquanto o numero NÃO for 10 continue
+// Se for diferente de 10 entra no loop
+while (rand !== 10) {
+    rand = random(min, max)
+    console.log(rand)
+}
+
