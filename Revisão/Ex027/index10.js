1f4a57e70ff35d14e1e6cec527c488bb8dc168fb
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-15 07:02:55 -0300

    Update index10.js

--- Revisão/Ex027/index10.js
+++ Revisão/Ex027/index10.js
@@ -3,9 +3,16 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for (let numero of numeros) {
 
     // Estamos pulando o numero 2 mas continuando adiante pulando para a proxima 
-    if (numero === 2) {
+    if (numero === 2 || numero === 5) {
         continue
     }
 
     console.log(numero)
+
+    // Vai quebrar no numero 7
+    // aqui vai sair do loop parando de execultar isso seria bom em um loop longo.
+    if (numero === 7) {
+        console.log('7 encontrado, saindo...')
+        break
+    }
 }
