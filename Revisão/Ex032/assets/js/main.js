f27510df301b335efa9a8478d9d83e7b9c607923
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-17 08:30:57 -0300

    timer

--- Revisão/Ex032/assets/js/main.js
+++ Revisão/Ex032/assets/js/main.js
@@ -1,11 +1,47 @@
-function iniciarTimer() {
-    console.log('teste')
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000)
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+
+    const relogio = document.querySelector('.relogio')
+    const iniciar = document.querySelector('.iniciar')
+    const pausar = document.querySelector('.pausar')
+    const zerar = document.querySelector('.zerar')
+    let segundos = 0
+    let timer
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++
+            relogio.innerHTML = criaHoraDosSegundos(segundos)
+        }, 1000)
+    }
+
+    document.addEventListener('click', function (evento) {
+        const el = evento.target
+
+        if (el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado')
+            clearInterval(timer)
+            iniciaRelogio()
+        }
+
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer)
+            relogio.classList.add('pausado')
+        }
+
+        if (el.classList.contains('zerar')) {
+            relogio.classList.remove('pausado')
+            clearInterval(timer)
+            relogio.innerHTML = '00:00:00'
+            segundos = 0
+        }
+    })
 }
 
-function pararTimer() {
-
-}
-
-function zerarTimer() {
-    
-}
+relogio()
