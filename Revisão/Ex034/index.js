36d76515deadff86f3f24713c05521597a121fc9
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-23 06:49:51 -0300

    Update index.js

--- Revisão/Ex034/index.js
+++ Revisão/Ex034/index.js
@@ -1,6 +1,15 @@
 // Declaração de função (Function hoisting)
+// Elas são "elevadas" no motor do js
+falaOi()
+
 function falaOi() {
     console.log('Oie')
+}
+
+// First- class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+    console.log('Sou um dado.')
 }
 
 
