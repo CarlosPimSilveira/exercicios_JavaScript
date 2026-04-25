a5684a1b61b413e443e74a289ce29072c290113a
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-24 21:43:46 -0300

    Update index1.js

--- Revisão/Ex034/index1.js
+++ Revisão/Ex034/index1.js
@@ -2,11 +2,11 @@ function funcao() {
     console.log('Oie')
     console.log(arguments)
     let total = 0
-    for (let argumento of argument) {
+    for (let argumento of arguments) {
         total += argumento
     }
     console.log(total)
 }
 // enviando argumento! mesmo a funcao nao esperando ele nao retorna erro!
-// quando defino uma function com a palavra function dentro dela tem a arguments que sustenta tudo que for enviado 
-funcao('valor', 1, 2, 3, 4, 5, 6, 7, 8, 9)
+// quando defino uma function com a palavra function dentro dela tem a arguments que sustenta tudo que for enviado 'valor', 
+funcao(1, 2, 3, 4, 5, 6, 7, 8, 9)
