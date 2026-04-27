f10bdca73f70ec1b38f9471b7a62486da51a1d09
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-27 19:11:23 -0300

    Update index3.js

--- Revisão/Ex034/index3.js
+++ Revisão/Ex034/index3.js
@@ -2,3 +2,12 @@ function funcao( {nome, sobrenome, idade} ) {
     console.log(nome, sobrenome, idade)
 }
 funcao( {nome: 'luiz', sobrenome: 'otávio', idade: 20} )
+
+function funcao1 ([valor1, valor2, valor3]) {
+    console.log(valor1, valor2, valor3)
+}
+funcao1(['Luiz otávio', 'miranda', 30])
+
+function conta(operador, acumulador, numeros) {
+    console.log(operador, acumulador, numeros)
+}
