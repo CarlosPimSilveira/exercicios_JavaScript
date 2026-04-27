2de3d2dd009e7dad80e7e60534f9ae04f4747a4d
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-27 18:49:59 -0300

    Update index1.js

--- Revisão/Ex034/index1.js
+++ Revisão/Ex034/index1.js
@@ -1,12 +1,12 @@
 // argumentos que sustenta todos os argumentos enviados!
-function funcao() {
+function funcao(a, b, c) {
     console.log('Oie')
     console.log(arguments)
     let total = 0;
     for (let argumento of arguments) {
         total += argumento
     }
-    console.log(total)
+    console.log(total, a, b, c)
 }
 // enviando argumento! mesmo a funcao nao esperando ele nao retorna erro!
 // quando defino uma function com a palavra function dentro dela tem a arguments que sustenta tudo que for enviado 'valor', 
