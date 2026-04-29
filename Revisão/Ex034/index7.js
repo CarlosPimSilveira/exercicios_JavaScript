9e67c0d0c5eac07df23987247c370bcd96c82618
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-29 06:47:15 -0300

    Update index7.js

--- Revisão/Ex034/index7.js
+++ Revisão/Ex034/index7.js
@@ -5,21 +5,24 @@ function rand(min = 1000, max = 3000) {
     return Math.floor(num)
 }
 
-function f1() {
+function f1(callback) {
     setTimeout(function() {
         console.log('f1')
+        if (callback) callback()
     }, rand())
 }
 
-function f2() {
+function f2(callback) {
     setTimeout(function() {
         console.log('f2')
+        if (callback) callback()
     }, rand())
 }
 
-function f3() {
+function f3(callback) {
     setTimeout(function() {
         console.log('f3')
+        if (callback) callback()
     }, rand())
 }
 
