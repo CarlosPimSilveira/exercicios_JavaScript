d190ef18d2d70843d0c073e8245bdb99de17513a
Carlos Pim <carlospimsilveira@gmail.com>
2026-04-29 06:43:07 -0300

    Update index7.js

--- Revisão/Ex034/index7.js
+++ Revisão/Ex034/index7.js
@@ -1,17 +1,24 @@
 // Funções de CallBack
 
 function f1() {
-    console.log('f1')
+    setTimeout(function() {
+        console.log('f1')
+    }, 1000)
 }
 
 function f2() {
-    console.log('f2')
+    setTimeout(function() {
+        console.log('f2')
+    }, 1000)
 }
 
 function f3() {
-    console.log('f3')
+    setTimeout(function() {
+        console.log('f3')
+    }, 1000)
 }
 
 f1()
 f2()
 f3()
+console.log('Olá mundo!')
