c69cde60a26f132265c8f35e591991fb776cd6df
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-05 20:01:45 -0300

    Update index9.js

--- Revisão/Ex034/index9.js
+++ Revisão/Ex034/index9.js
@@ -1,7 +1,12 @@
 // Funções fabrica (factory functions)
+// Quando uma função esta dentro de um objeto chama metodo
 function criaPessoa(nome, sobrenome) {
     return {
-        nome, sobrenome
+        nome, 
+        sobrenome,
+        fala: function(assunto) {
+            return `${nome} está falando ${assunto}`
+        }
     }
 }
 
