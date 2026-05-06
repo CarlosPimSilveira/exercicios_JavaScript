29a3580a397948cfb951c09c1135b4e65fdedc41
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-06 06:39:36 -0300

    Update index9.js

--- Revisão/Ex034/index9.js
+++ Revisão/Ex034/index9.js
@@ -1,19 +1,19 @@
 // Funções fabrica (factory functions)
 // Quando uma função esta dentro de um objeto chama metodo
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome, 
         sobrenome,
         fala(assunto) {
             return `${this.nome} está falando ${assunto}`
         }, 
-        latura,
-        peso
+        latura: altura,
+        peso: peso
     };
 }
 
-const p1 = criaPessoa('Luiz', 'Otavio')
-const p2 = criaPessoa('Maria', 'Eduarda')
+const p1 = criaPessoa('Luiz', 'Otavio', 1.8, 80)
+const p2 = criaPessoa('Maria', 'Eduarda', 1.6, 52)
 
 // Mesma coisa - this é quem chama! nesse caso! o objeto se refere a quem esta chamando!
 console.log(p1.fala('falando sobre JS'))
