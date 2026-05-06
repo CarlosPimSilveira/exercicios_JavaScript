b675b0b303a9a4a3a3af7826bec363a3f28c76d6
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-06 06:57:44 -0300

    Update index9.js

--- Revisão/Ex034/index9.js
+++ Revisão/Ex034/index9.js
@@ -4,12 +4,21 @@ function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome, 
         sobrenome,
+        // getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ')
+            console.log(valor)
+        },
         fala(assunto) {
             return `${this.nome} está falando ${assunto}`
         }, 
         altura: altura,
         peso: peso,
-        // Começa a "fingir" ser um atributo da classe!
+        // Começa a "fingir" ser um atributo da classe! Getter
         get imc() {
             const indice = this.peso / (this.altura ** 2)
             return indice.toFixed(2)
@@ -22,6 +31,8 @@ const p2 = criaPessoa('Maria', 'Eduarda', 1.6, 52)
 console.log(p1.nome)
 console.log(p1.altura)
 console.log(p1.imc)
+p1.nomeCompleto = 'Maria Oliveira Silva'
+console.log(p1.nomeCompleto)
 // Mesma coisa - this é quem chama! nesse caso! o objeto se refere a quem esta chamando!
 console.log(p1.fala('falando sobre JS'))
 console.log(p2.fala('teste'))
