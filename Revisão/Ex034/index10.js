4fd0c7e6f06b574ef017c001173a0be443a48d91
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-07 20:48:11 -0300

    Update index10.js

--- Revisão/Ex034/index10.js
+++ Revisão/Ex034/index10.js
@@ -1,17 +1,28 @@
+let numAleatorio = rand()
+
 function adivinharNum() {
     let txtNum = document.querySelector('.txtNum')
     let numReal = Number(txtNum.value)
-    let numAleatorio = 6//rand()
     let respostas = document.querySelector('.respostas')
+    respostas.innerHTML = ''
 
     if (isNaN(numReal)) {
-        window.alert('Digite um numero!'); 
+        window.alert('Digite um numero!');
         return
     }
 
     if (numReal === numAleatorio) {
         const p = document.createElement('p')
         p.innerText = 'Certo!'
+        respostas.appendChild(p);
+        numAleatorio = rand()
+    } else if (numReal < numAleatorio) {
+        const p = document.createElement('p')
+        p.innerText = 'Um numero um pouco maior!'
+        respostas.appendChild(p);
+    } else {
+        const p = document.createElement('p')
+        p.innerText = 'Um numero um pouco menor!'
         respostas.appendChild(p);
     }
     console.log(numAleatorio)
@@ -23,3 +34,10 @@ function rand(min = 1, max = 10) {
     const num = Math.random() * (max - min) + min
     return Math.floor(num)
 }
+
+/*
+    if (numReal === numAleatorio) {
+        const p = document.createElement('p')
+        p.innerText = 'Certo!'
+        respostas.appendChild(p);
+    }*/
