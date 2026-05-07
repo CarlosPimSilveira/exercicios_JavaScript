e594fa038c1334588ad7f87a7b9c8b680b21dbe0
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-07 20:22:23 -0300

    Update index10.js

--- Revisão/Ex034/index10.js
+++ Revisão/Ex034/index10.js
@@ -1,3 +1,22 @@
 function adivinharNum() {
-    
+    let txtNum = document.querySelector('.txtNum')
+    let numAleatorio = rand()
+
+    if (isNaN(Number(txtNum.value))) {
+        window.alert('Digite um numero!'); 
+        return
+    }
+
+    if (true) {
+        const p = document.createElement('p')
+        p.innerText = 'Certo!'
+        p.appendChild;
+    }
+    txtNum === numAleatorio
+    txtNum.value = ''
+}
+
+function rand(min = 1, max = 10) {
+    const num = Math.random() * (max - min) + min
+    return Math.floor(num)
 }
