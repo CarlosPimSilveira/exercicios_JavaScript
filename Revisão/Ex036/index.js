1bb2bacc13f64680a18f5970473082f632b7e43f
Carlos Pim <carlospimsilveira@gmail.com>
2026-05-11 20:09:35 -0300

    Update index.js

--- Revisão/Ex036/index.js
+++ Revisão/Ex036/index.js
@@ -1,20 +1,32 @@
 function criaElemento() {
-    const existe = document.querySelector('.minhaDiv')
-    if (!existe)
-        criaDivTarefas()
+    let divTarefa = document.querySelector('.tarefaDiv')
+    
+    if (!divTarefa) {
+        divTarefa = criaDivTarefas()
+    }
 
-    const 
+    let lista = document.querySelector('.olTarefa')
+    if (!lista) {
+        lista = criaLista(divTarefa)
+    }
 
     criaTarefa()
-
 }
 
 function criaDivTarefas() {
     const div = document.createElement('div')
-    div.classList.add('minhaDiv')
+    div.classList.add('tarefaDiv')
     document.body.appendChild(div)
+    return div
+}
+
+function criaLista(divTarefa) {
+    const lista = document.createElement('ol')
+    lista.classList.add('olTarefa')
+    divTarefa.appendChild(lista)
+    return lista
 }
 
 function criaTarefa(){
-    const 
+    //const  
 }
