22d2cee6b1515ecd36bc97ec50b6efaa46202389
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-03 06:53:52 -0300

    Update main.js

--- Revisão/Ex037/assets/js/main.js
+++ Revisão/Ex037/assets/js/main.js
@@ -1,8 +1,15 @@
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
+
         inicia() {
             alert('Oi, iniciei')
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function(e) {
+                const el = e.target
+            });
         },
     };
 }
