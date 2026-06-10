e3f6082933c9d8498ff4135f1db1c3c34bec1124
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-10 19:41:18 -0300

    estudos

---
+++ Revisão/Ex038/index.js
@@ -0,0 +1,9 @@
+/* 
+    Exemplo ao digitar 3 ele deve retornar um array com A, B, C
+    20 - A, B, C, D, E, F, G, H e por ai vai ate o numero 20 do abc
+*/
+
+function abc() {
+    let Dig = document.querySelector('.abc')
+    console.log(Dig)
+}
