4cfefbe40d48d90d381604fb8fd999e93c49be73
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-11 20:35:34 -0300

    fundo completo

--- Revisão/Ex038/index.js
+++ Revisão/Ex038/index.js
@@ -25,3 +25,35 @@ function abc() {
     campoFocus.value = ''
     campoFocus.focus()
 }
+
+const starsContainer = document.getElementById('stars');
+
+for (let i = 0; i < 150; i++) {
+    const star = document.createElement('div');
+
+    star.classList.add('star');
+
+    const size = Math.random() * 3 + 1;
+
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.top = `${Math.random() * 100}vh`;
+
+    starsContainer.appendChild(star);
+
+    piscar(star);
+}
+
+function piscar(star) {
+    function alterar() {
+        star.style.opacity = Math.random() > 0.5 ? '1' : '0';
+
+        const proximaMudanca = Math.random() * 3000 + 500;
+
+        setTimeout(alterar, proximaMudanca);
+    }
+
+    alterar();
+}
