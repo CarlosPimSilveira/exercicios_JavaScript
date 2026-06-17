dd507dec19df2e2f4a18f9368bc11bbc474c3f11
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-17 16:13:58 -0300

    Update index1.js

--- Revisão/Ex039/index1.js
+++ Revisão/Ex039/index1.js
@@ -1,10 +1,10 @@
 // Funções recursivas 
 // "Ela mesmo se chama"
 function recursiva (max){
-    if (max > 10) return
-    max++
-    console.log(max)
-    recursiva(max)
+    if (max > 10) return //para aqui quando for maior que 10
+    max++ // max que começa no 0 sobe 1
+    console.log(max) // exibe na tela
+    recursiva(max) //chama ela mesma passando o mesmo parametro
 }
 
 recursiva(0)
