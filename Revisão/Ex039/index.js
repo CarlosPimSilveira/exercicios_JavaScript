9e824e9d750fd94c4d3c847d8fded126fa6ba965
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-16 22:30:07 -0300

    Update index.js

--- Revisão/Ex039/index.js
+++ Revisão/Ex039/index.js
@@ -3,7 +3,21 @@
 // Função fabrica -> objetos
 // construtora -> deve sempre iniciar com letra maiuscula (new)
 
+function Pessoa(nome, sobrenome) {
+    // Atriubutos ou métodos privadas!
+    const ID = 123456;
+    const metodoInterno = function() {
 
+    };
+
+    // Atriubutos ou métodos publicos!
+    this.nome = nome
+    this.sobrenome = sobrenome
+
+    this.metodo = function() {
+        console.log( this.nome + ' Sou um método!')
+    }
+} // Pessoa.nome = 
 
 const p1 = new Pessoa('Carlos', 'Pim')
 const p2 = new Pessoa('Maria', 'Oliveira')
