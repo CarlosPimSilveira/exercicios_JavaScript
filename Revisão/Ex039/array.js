413dd1d24cfd6f789181619e591accdd4bd332b5
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-18 12:06:03 -0300

    Update array.js

--- Revisão/Ex039/array.js
+++ Revisão/Ex039/array.js
@@ -6,4 +6,8 @@ delete nomes[2] // remover o item do array sem mexer nos indices!
 console.log(nomes)
 // String, Objetos, Funções, Números!
 // Construtor
-const nome = new Array('Eduardo', 'Maria', 'Joana')
+//const nome = new Array('Eduardo', 'Maria', 'Joana')
+// Valor por referência
+const novo = nomes // num é copia é apontar para o mesmo local da memoria!
+novo.pop() // ambos perdem pq ambos apontam para o mesmo local na memoria!
+console.log(nomes, novo)
