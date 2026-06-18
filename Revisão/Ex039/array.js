56ff62e3abb56c8c40d13aa69e055b58c0031138
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-18 12:18:30 -0300

    Update array.js

--- Revisão/Ex039/array.js
+++ Revisão/Ex039/array.js
@@ -16,5 +16,6 @@ const novos = [...nomes]
 novos[2] = 'João' // agora como ambos não estão ligados posso alterar 1 sem afetar o outro!
 console.log(nomes, novos)
 console.log(novos.length)
-const removido = novos.pop()
+const removido1 = novos.pop() //final
+const removido2 = novos.shift() // começo
 console.log(removido, novos)
