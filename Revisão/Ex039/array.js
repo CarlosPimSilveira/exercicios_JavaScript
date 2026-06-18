603309c35c8a01b888d427596fbded760c45eb64
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-18 12:04:30 -0300

    Update array.js

--- Revisão/Ex039/array.js
+++ Revisão/Ex039/array.js
@@ -2,5 +2,8 @@
 //                 0         1        2 
 const nomes = ['Eduardo', 'Maria', 'Joana']
 nomes[2] = 'João'
-delete nomes[2]
+delete nomes[2] // remover o item do array sem mexer nos indices!
 console.log(nomes)
+// String, Objetos, Funções, Números!
+// Construtor
+const nome = new Array('Eduardo', 'Maria', 'Joana')
