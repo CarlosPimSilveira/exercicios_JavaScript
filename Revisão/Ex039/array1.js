742883e9a071cc9d7a324c79e53debed50989f0e
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-19 08:26:54 -0300

    Update array1.js

--- Revisão/Ex039/array1.js
+++ Revisão/Ex039/array1.js
@@ -2,5 +2,7 @@
 //                 0         1        2         3          4 
 const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
 const novo = nomes.slice(1, 3) // "fatia" o array começa na maria e vai ate joana!
+const novo1 = nomes.slice(1, -1)
 console.log(nomes)
 console.log(novo)
+console.log(novo1)
