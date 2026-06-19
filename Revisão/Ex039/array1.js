3dc6c2fe464e00e332b3b3c4ed210fe1f34d2fca
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-19 08:26:13 -0300

    Update array1.js

--- Revisão/Ex039/array1.js
+++ Revisão/Ex039/array1.js
@@ -1,4 +1,6 @@
 // Valor por referência
 //                 0         1        2         3          4 
 const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+const novo = nomes.slice(1, 3) // "fatia" o array começa na maria e vai ate joana!
 console.log(nomes)
+console.log(novo)
