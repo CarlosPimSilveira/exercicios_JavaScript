aa5c2d8b88854d9aa1ee4827acc67a0e42559cbc
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-20 09:40:16 -0300

    estudos

--- Revisão/Ex039/Splice.js
+++ Revisão/Ex039/Splice.js
@@ -9,5 +9,18 @@ const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
 // splice(-1, 1) remove a julia pois ela é o -1 e to removendo 1 
 // splice(-2, 1) remove o gabriel pois ele é indice -2 e to removendo apenas 1
 // splice(-2, Number.MAX_VALUE) tire do -2(gabriel) ate o final (max_value) é um numero maximo que o js suporta
-const removidos = nomes.splice(-2, 1)
-console.log(nomes, removidos)
+// splice(3, 0, 'carlos') -> ele vai ate o indice 3(gabriel) e adiciona o (carlos) agora carlos é o indice 3 e o gabriel passa a ser o indice 4 com isso julia tambem se move para o indice 5 e por ai vai!
+// nomes.splice(3, 1, 'Carlos') -> nesse caso ele remove o gabriel salva em removidos e no local adiciona carlos fazendo carlos ser o indice 3 e julia continuar no indice 4
+// simular pop()
+const removidos = nomes.splice(-1, 1) // removi a julia e salvei no removidos
+
+// shift
+const removidos1 = nomes.splice(0, 1) // removi a maria e ela foi para o removidos1
+
+// push
+nomes.splice(nomes.length, 0, 'Luiz', 'Otávio', 'Miranda') // adiciona no final do array!
+
+// unshift
+nomes.splice(0, 0, 'Eduardo', 'Pim', 'Larissa') // adiciona no começo do array!
+
+console.log(nomes, removidos, removidos1)
---
+++ Revisão/Ex040/index.js
@@ -0,0 +1,20 @@
+const txtArray = document.querySelector('.txtArray') 
+const btnExibir = document.querySelector('.btnExibir')
+const resposta = document.createElement('p')
+const revArray = []
+
+if (revArray.length === 0) {
+    btnExibir.disabled = true
+}
+
+function enviar()  {
+    revArray.splice(revArray.length, 0, txtArray.value)
+    btnExibir.disabled = false
+    txtArray.value = ''
+    txtArray.focus()
+}
+
+function exibir() {
+    resposta.innerText = revArray.toString()
+    document.querySelector('.divCent').appendChild(resposta)
+}
