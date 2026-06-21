988b76ed7ee19c739c117a368b0f1683f680afbe
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-21 10:29:22 -0300

    Update reduce.js

--- Revisão/Ex039/reduce.js
+++ Revisão/Ex039/reduce.js
@@ -27,13 +27,12 @@ const pessoas = [
     { nome: 'Luiz', idade: 62 },
     { nome: 'Maria', idade: 23 },
     { nome: 'Eduardo', idade: 55 },
-    { nome: 'Letícia', idade: 19 },
-    { nome: 'Rosana', idade: 32 },
-    { nome: 'Wallace', idade: 47 },
+    { nome: 'Letícia', idade: 65 },
+    { nome: 'Rosana', idade: 90 },
+    { nome: 'Wallace', idade:63 },
 ]
-const pessoaVelha = pessoas.reduce(function(acumulador, valor) {
-    if (acumulador <= valor.idade)
-        acumulador = valor.idade
-    return acumulador
-}, 0)
-console.log(pessoaVelha)
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+    if (acumulador.idade > valor.idade) return acumulador 
+    return valor
+})
+console.log(maisVelha)
