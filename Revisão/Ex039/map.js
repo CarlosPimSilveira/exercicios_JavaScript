2a065f1770910c7c3d34e658d747009e4a4e2365
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-21 00:52:13 -0300

    Create map.js

---
+++ Revisão/Ex039/map.js
@@ -0,0 +1,20 @@
+// Map - Mapeando o array
+
+//               0  1   2   3  4  5  6  7  8  9   10  11  12  13  14... 
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27, 10, 15, 20, 22, 29, 63, 99, 14, 15]
+
+
+
+
+
+
+
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
