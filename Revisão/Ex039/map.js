4454687586ac07a44563076720088e78c2731dfc
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-21 00:56:38 -0300

    Update map.js

--- Revisão/Ex039/map.js
+++ Revisão/Ex039/map.js
@@ -1,7 +1,9 @@
 // Map - Mapeando o array
-
+// Dobre os numeros
 //               0  1   2   3  4  5  6  7  8  9   10  11  12  13  14... 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27, 10, 15, 20, 22, 29, 63, 99, 14, 15]
+const numerosEmdobro = numeros.map(valor => valor * 2)
+console.log(numerosEmdobro)
 
 
 
