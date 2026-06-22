2283d5f4abb34e86e50fbc2c8366feef2eb7e73f
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-22 08:27:55 -0300

    estudos

---
+++ Revisão/Ex039/forEach.js
@@ -0,0 +1,18 @@
+// forEach!
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+let total = 0
+a1.forEach(valor => {
+    total += valor
+})
+console.log(total)
+
+a1.forEach(valor => console.log(valor))
+
+a1.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+})
+
+/*
+for(let valor of a1) {
+    console.log(valor)
+} */
