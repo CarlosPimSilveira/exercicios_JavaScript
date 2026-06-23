92fa46013176d254ba1af8beaf082ad1b1d903be
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-23 11:57:38 -0300

    Update revObj2.js

--- Revisão/Ex039/revObj2.js
+++ Revisão/Ex039/revObj2.js
@@ -1,3 +1,13 @@
 // Factory functions / Constructor functions / Classes
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome, 
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
+}
 
-
+const p1 = criaPessoa('Carlos', 'Pim');
+console.log(p1.nomeCompleto);
