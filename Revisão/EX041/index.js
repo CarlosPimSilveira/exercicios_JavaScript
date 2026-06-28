9e1e560694a3abe0d8786f008ead3eb442212f77
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-28 10:25:41 -0300

    Update index.js

--- Revisão/EX041/index.js
+++ Revisão/EX041/index.js
@@ -3,16 +3,37 @@
 // função construtora
 function Produto(nome, preco, estoque) {
     // publico
+    /*
     this.nome = nome
     this.preco = preco
-
+    */
     Object.defineProperty(this, 'estoque', {
         enumerable: true,    // Mostra a chave
         value: estoque,      // Mostra o valor da chave
         writable: false,     // impede de mudar o valor do estoque - pode alterar ou não
         configurable: false  // pode apagar a chave, pode reconfigurar a chave - configuravel
     })
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,    // Mostra a chave
+            value: nome,      // Mostra o valor da chave
+            writable: true,     // impede de mudar o valor do estoque - pode alterar ou não
+            configurable: true  // pode apagar a chave, pode reconfigurar a chave - configuravel
+        },
+        preco: {
+            enumerable: true,    // Mostra a chave
+            value: preco,      // Mostra o valor da chave
+            writable: true,     // impede de mudar o valor do estoque - pode alterar ou não
+            configurable: true  // pode apagar a chave, pode reconfigurar a chave - configuravel
+        }
+    })
 }
 
 const p1 = new Produto('Camiseta', 20, 3)
 console.log(p1)
+console.log(Object.keys(p1))
+
+for(let chave in p1) {
+    console.log(chave)
+}
