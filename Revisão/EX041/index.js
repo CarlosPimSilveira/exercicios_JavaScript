f41e481bb92ab0272acfbd7d2ab0cdb811376d19
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-28 10:05:35 -0300

    Update index.js

--- Revisão/EX041/index.js
+++ Revisão/EX041/index.js
@@ -1,2 +1,8 @@
 // Object.defineProperty() e object.defineProperties()
 
+// função construtora
+function Produto(nome, preco, estoque) {
+    this.nome = nome
+}
+
+const p1 = new Produto('Camiseta', 20, 3)
