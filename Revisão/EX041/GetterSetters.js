002ffaff148c98c9c5d4f83fe93b0cce573fd71f
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-29 10:03:07 -0300

    Update GetterSetters.js

--- Revisão/EX041/GetterSetters.js
+++ Revisão/EX041/GetterSetters.js
@@ -7,6 +7,8 @@ function Produto(nome, preco, estoque) {
     // publico
     this.nome = nome
     this.preco = preco
+
+    let estoquePrivado = estoque
     Object.defineProperty(this, 'estoque', {
         enumerable: true,    // Mostra a chave
         // value: estoque,      // Mostra o valor da chave
@@ -16,13 +18,17 @@ function Produto(nome, preco, estoque) {
             return estoque
         },
         set: function(valor) {
-            
+            if (typeof valor !== 'number') {
+                console.log('Bad value')
+                return
+            }
+            estoquePrivado = valor
         }
     })
 }
 
 const p1 = new Produto('Camiseta', 20, 3)
-console.log(p1)
+//console.log(p1)
 p1.estoque = 'O valor que eu quero'
 console.log(p1.estoque)
 
