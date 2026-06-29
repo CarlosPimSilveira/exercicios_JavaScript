5171c1d188d1ba0296bea749862bc060f5673013
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-29 09:51:34 -0300

    Create GetterSetters.js

---
+++ Revisão/EX041/GetterSetters.js
@@ -0,0 +1,17 @@
+// Object.defineProperty() e object.defineProperties()
+
+// função construtora
+function Produto(nome, preco, estoque) {
+    // publico
+    this.nome = nome
+    this.preco = preco
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,    // Mostra a chave
+        value: estoque,      // Mostra o valor da chave
+        writable: false,     // impede de mudar o valor do estoque - pode alterar ou não
+        configurable: false  // pode apagar a chave, pode reconfigurar a chave - configuravel
+    })
+}
+
+const p1 = new Produto('Camiseta', 20, 3)
+console.log(p1)
