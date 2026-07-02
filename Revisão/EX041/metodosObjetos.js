dfe35bb5924c041bf2b3cf2fc2b664bd40ddce08
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-02 15:50:38 -0300

    Update metodosObjetos.js

--- Revisão/EX041/metodosObjetos.js
+++ Revisão/EX041/metodosObjetos.js
@@ -16,7 +16,10 @@ const produto = {
     nome: 'Caneca',
     preco: 1.8
 }
-const outraCoisa = {...produto}
+const outraCoisa = {
+    ...produto, 
+    material: 'porcelana'
+}
 
 outraCoisa.nome = 'Outro nome'
 outraCoisa.preco = 2.5
