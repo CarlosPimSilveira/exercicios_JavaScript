a0b6d5e57613b8e86f560f378e6ede8c01821185
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-04 19:04:16 -0300

    Update metodosObjetos2.js

--- Revisão/EX041/metodosObjetos2.js
+++ Revisão/EX041/metodosObjetos2.js
@@ -1,4 +1,13 @@
 const produto = { nome: 'Produto', preco: 1.8 }
-console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
+Object.defineProperty(produto, 'nome', {
+    writable: false,
+    configurable: false,
+    value: 'Qualquer outra coisa'
+})
 
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
+// Apenas o nome do produto esta configurado e nao vai mudar ou aceitar mudanças porem o preço vai
+produto.preco = 'Outra coisa'
+delete produto.nome 
+console.log(produto)
 
