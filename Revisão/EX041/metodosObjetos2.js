177b2be7ab2d78db5d25ef5b5b66d35653b9d6bb
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-05 14:37:16 -0300

    Update metodosObjetos2.js

--- Revisão/EX041/metodosObjetos2.js
+++ Revisão/EX041/metodosObjetos2.js
@@ -8,4 +8,5 @@ Object.defineProperty(produto, 'nome', {
 console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
 // Apenas o nome do produto esta configurado e nao vai mudar ou aceitar mudanças porem o preço vai
 produto.preco = 'Outra coisa'
+delete produto.nome 
 
