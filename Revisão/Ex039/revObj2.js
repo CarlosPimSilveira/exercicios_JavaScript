60664f327aeb2ac4df2c33da60ba06d569071255
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-06 21:55:58 -0300

    Update revObj2.js

--- Revisão/Ex039/revObj2.js
+++ Revisão/Ex039/revObj2.js
@@ -12,9 +12,3 @@ const p1 = criaPessoa('Carlos', 'Pim');
 console.log(p1.nomeCompleto); 
 
 
-function Pessoa(nome, sobrenome) {
-    this.nome = nome
-    this.sobrenome = sobrenome
-} // palavra new cria um objeto vazio {} - this não é uma variavel! -> cria um novo objeto e atrela com o this ao novo objeto!
-const p2 = new Pessoa('Luiz', 'Mirandaa')
-console.log(p2)
