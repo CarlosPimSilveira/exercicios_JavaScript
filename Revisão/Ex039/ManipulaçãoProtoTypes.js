87945edfe44e140708670f73e8fd29046626ce38
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-13 10:20:00 -0300

    Update ManipulaçãoProtoTypes.js

--- Revisão/Ex039/ManipulaçãoProtoTypes.js
+++ Revisão/Ex039/ManipulaçãoProtoTypes.js
@@ -1,4 +1,14 @@
-// new Object
+// new Object -> Object.prototype
 const objA = {
     chaveA: 'A'
+    // __proto__: Object.prototype
 }
+
+// new Object -> Object.prototype
+const objB = {
+    chaveA: 'B'
+    // __proto__: Object.prototype
+}
+
+Object.setPrototypeOf(objB, objA)
+console.log(objB.chaveA)
