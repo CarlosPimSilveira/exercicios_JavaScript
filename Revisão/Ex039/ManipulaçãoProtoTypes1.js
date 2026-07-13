a10f4083451e6fd03066f5074d9faa1306094fb1
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-13 10:42:43 -0300

    Update ManipulaçãoProtoTypes1.js

--- Revisão/Ex039/ManipulaçãoProtoTypes1.js
+++ Revisão/Ex039/ManipulaçãoProtoTypes1.js
@@ -12,5 +12,14 @@ Produto.prototype.aumento = function(percentual) {
 }
 
 const p1 = new Produto('Camiseta', 50)
+// Literal
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+} // Agora o p2 caneca consegue ter acesso ao prototype do produto podendo fazer o mesmo e ainda é exibida como se o contrutor fosse a function produto que é contrutora
+Object.setPrototypeOf(p2, Produto.prototype)
+
 p1.aumento(5)
 console.log(p1)
+p2.aumento(25)
+console.log(p2)
