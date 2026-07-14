762280e06836e05cc08d50de394a74866ae23175
Carlos Pim <carlospimsilveira@gmail.com>
2026-07-14 09:55:35 -0300

    Update herança.js

--- Revisão/Ex042/herança.js
+++ Revisão/Ex042/herança.js
@@ -16,8 +16,18 @@ Produto.prototype.desconto = function(quantia) {
 
 function Camiseta(nome, preco, cor) {
     Produto.call(this, nome, preco)
+    this.cor = cor
+}
+// Camiseta.prototype.constructor = Camiseta
+Camiseta.prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta // Corrigindo quem realmente é o construtor sem isso o construtor ficaria sendo produto
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100))
 }
 
+const produto = new Produto('Gen', 111)
 const camiseta = new Camiseta('Regata', 7.5, 'Preta')
 camiseta.aumento(10)
 console.log(camiseta)
+console.log(produto)
