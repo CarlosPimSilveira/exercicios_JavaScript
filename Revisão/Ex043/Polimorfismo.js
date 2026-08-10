c2f56eddeded281585980a67822abc212f194f0b
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-10 18:34:04 -0300

    Update Polimorfismo.js

--- Revisão/Ex043/Polimorfismo.js
+++ Revisão/Ex043/Polimorfismo.js
@@ -48,17 +48,3 @@ function ContaPoupanca(agencia, conta, saldo) {
     Conta.call(this, agencia, conta, saldo)
 }
 
-ContaPoupanca.prototype = Object.create(Conta.prototype)
-ContaPoupanca.prototype.constructor = ContaPoupanca
-
-const cc = new ContaCorrente(11, 22, 0, 100)
-cc.depositar(10)
-cc.sacar(110)
-cc.sacar(1)
-
-console.log()
-
-const cp = new ContaPoupanca(12, 33, 0)
-cp.depositar(10)
-cp.sacar(110)
-cp.sacar(1)
