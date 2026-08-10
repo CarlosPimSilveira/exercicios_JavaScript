55acd3467e00567103a1a1dc6b091925a322e749
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-10 19:52:49 -0300

    Update FactoryFunctions.js

--- Revisão/Ex043/FactoryFunctions.js
+++ Revisão/Ex043/FactoryFunctions.js
@@ -1,17 +1,24 @@
-function criaPessoa(nome, sobrenome) {
-
-    const pessoaPrototype = {
-        falar() {
+const falar = {
+    falar() {
             console.log(`${this.nome} está falando.`)
-        },
-        comer() {
-            console.log(`${this.nome} está comendo.`)
-        },
-        beber() {
-            console.log(`${this.nome} está bebendo.`)
-        },
-    }
+        }
+}
 
+const comer = {
+    comer() {
+            console.log(`${this.nome} está comendo.`)
+        }
+}
+
+const beber = {
+    beber() {
+            console.log(`${this.nome} está bebendo.`)
+        }
+}
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber)
+
+function criaPessoa(nome, sobrenome) {
     return Object.create(pessoaPrototype, {
         nome: {
             value: nome
