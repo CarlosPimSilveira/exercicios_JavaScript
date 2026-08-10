56ddf4dd9804dca86ac54cb79eeb285d9fed9da3
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-10 06:20:48 -0300

    Update Polimorfismo.js

---
+++ Revisão/Ex043/Polimorfismo.js
@@ -0,0 +1,29 @@
+// Superclass
+function Conta(agencia, conta, saldo) {
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor) {
+    if(valor > this.saldo) {
+        console.log(`Saldo insuficiente: ${this.saldo}`)
+        return
+    }
+    
+    this.saldo -= valor
+    this.verSaldo()
+}
+
+Conta.prototype.depositar = function(valor) {
+    this.saldo += valor
+    this.verSaldo()
+}
+
+Conta.prototype.verSaldo = function() {
+    console.log(`Ag.: ${this.agencia}/${this.conta} | ` + 
+        `Saldo: ${this.saldo.toFixed(2)}`
+    )
+}
+
+
