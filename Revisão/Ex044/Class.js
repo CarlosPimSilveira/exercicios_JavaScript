e178f2d0586a3211ce21905b8b7c5316000d3b57
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-12 06:14:21 -0300

    Update Class.js

--- Revisão/Ex044/Class.js
+++ Revisão/Ex044/Class.js
@@ -19,8 +19,19 @@ class Pessoa{
     }
 }
 
+//mesma coisa so que function construtora
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+
+Pessoa2.prototype.falar = function() {
+    console.log(`${this.nome} está falando.`)
+}
+
 const p1 = new Pessoa('Luiz', 'Miranda')
 const p2 = new Pessoa('Maria', 'Miranda')
 const p3 = new Pessoa('Joana', 'Miranda')
 const p4 = new Pessoa('João', 'Miranda')
+const p5 = new Pessoa2('Carlos', 'Jose')
 console.log(p1)
