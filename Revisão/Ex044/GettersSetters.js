8e9ce9a86ce7e48918990a636f1858e04a99adfc
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-13 06:10:35 -0300

    Update GettersSetters.js

--- Revisão/Ex044/GettersSetters.js
+++ Revisão/Ex044/GettersSetters.js
@@ -8,4 +8,17 @@ class Carro {
         if (this.velocidade >= 100) return
         this.velocidade++
     }
+
+    freiar() {
+        if (this.velocidade <= 0) return
+        this.velocidade--;
+    }
 }
+
+const c1 = new Carro('Fusca')
+for (let i = 0; i <= 200; i++) {
+    c1.acelerar()
+}
+
+console.log(c1)
+
