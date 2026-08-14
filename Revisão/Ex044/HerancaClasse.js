f57bdd9fe0e1091a443d3caf9a2e241ecade1121
Carlos Pim <carlospimsilveira@gmail.com>
2026-08-14 18:45:29 -0300

    Update HerancaClasse.js

--- Revisão/Ex044/HerancaClasse.js
+++ Revisão/Ex044/HerancaClasse.js
@@ -1,6 +1,29 @@
-class DispositivoEletronico() {
+class DispositivoEletronico {
     constructor(nome) {
-        this.nome = nome
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    //Metodos
+    ligar() {
+        if(this.ligado) {
+            console.log(this.nome +' Já ligado')
+            return
+        }
+
+        this.ligado = true
+    }
+
+    desligar() {
+        if(!this.ligado) {
+            console.log(this.nome +' Já desligado')
+            return
+        }
+
         this.ligado = false
     }
 }
+
+const d1 = new DispositivoEletronico('Smartphone')
+console.log(d1.ligar)
+
