5a5b8aa63ff9a672f245802b0a629259cb80d6d6
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-01 06:51:25 -0300

    Create index.js

---
+++ Revisão/Ex045/index.js
@@ -0,0 +1,11 @@
+// Usando classes validando CPF
+/* 
+705.484.450-52 070.987.720-03
+*/
+class ValidaCPF{
+    constructor(cpfEnviado) {
+        Object.defineProperty()
+    }
+}
+
+const validacpf = new ValidaCPF('070.987.720-03')
