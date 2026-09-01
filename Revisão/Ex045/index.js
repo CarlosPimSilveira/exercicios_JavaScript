4a7b1fe506af0d526df6562440846cdbe24d2b79
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-01 06:57:00 -0300

    Update index.js

--- Revisão/Ex045/index.js
+++ Revisão/Ex045/index.js
@@ -4,8 +4,26 @@
 */
 class ValidaCPF{
     constructor(cpfEnviado) {
-        Object.defineProperty()
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    éSequência() {
+        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length)
+    }
+
+    valida() {
+        if(!this.cpfLimpo) return false
+        if(typeof this.cpfLimpo !== 'string') return falso
+        if(this.cpfLimpo.length !== 11) return falso
+
+        return 'Cheguei aqui'
     }
 }
 
 const validacpf = new ValidaCPF('070.987.720-03')
+console.log(validacpf.valida())
