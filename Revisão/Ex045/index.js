226e0ccc7645c0bba1f23ef9bcd8a60576cf4241
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-01 07:11:37 -0300

    Update index.js

--- Revisão/Ex045/index.js
+++ Revisão/Ex045/index.js
@@ -13,13 +13,31 @@ class ValidaCPF{
     }
 
     éSequência() {
-        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length)
+        // Esta verificando se é uma sequencia!
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+        const digito1 = this.geraDigito(cpfSemDigitos)
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1)
+    }
+
+    geraDigito(cpfSemDigitos) {
+        let total = 0
+        let reverso = cpfSemDigitos.length + 1
+
+        for(let stringNumerica of cpfSemDigitos) {
+            console.log(stringNumerica, typeof stringNumerica)
+        }
     }
 
     valida() {
         if(!this.cpfLimpo) return false
         if(typeof this.cpfLimpo !== 'string') return falso
         if(this.cpfLimpo.length !== 11) return falso
+        if(this.éSequência()) return false
+        if(!this.geraNovoCpf()) return false
 
         return 'Cheguei aqui'
     }
