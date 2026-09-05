334a91217a68f44738d6b69d902f67d2914bb1b8
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-04 22:01:25 -0300

    Update main.js

---
+++ Revisão/Ex046/assets/js/main.js
@@ -0,0 +1,17 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario')
+
+
+        this.eventos()
+    }
+
+    // Metodos!
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.
+        })
+    }
+}
+
+const valida = ValidaFormulario()
