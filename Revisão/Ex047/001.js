64089b4d864ee3f33e8d3f9a87e1d55945cf5925
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-13 22:03:57 -0300

    Update 001.js

--- Revisão/Ex047/001.js
+++ Revisão/Ex047/001.js
@@ -3,3 +3,17 @@ function rand(min, max) {
     max *= 1000
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof msg !== 'string'){
+                reject('Cai no erro')
+                return
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise')
+            return
+        }, tempo);
+    })
+}
