66af874a5dd90cb44e73b7f52b0512c062a8e829
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-14 21:11:51 -0300

    Update Async-Await.js

--- Revisão/Ex047/Async-Await.js
+++ Revisão/Ex047/Async-Await.js
@@ -4,16 +4,3 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-function esperaAi(msg, tempo) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(typeof msg !== 'string'){
-                reject('Cai no erro')
-                return
-            }
-
-            resolve(msg.toUpperCase() + ' - Passei na promise')
-            return
-        }, tempo);
-    })
-}
