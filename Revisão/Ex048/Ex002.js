c94594fb88aa6057771dfd33e1529ba7cf5589eb
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-22 21:31:26 -0300

    Update Ex002.js

--- Revisão/Ex048/Ex002.js
+++ Revisão/Ex048/Ex002.js
@@ -259,16 +259,7 @@ for (let c = 1; c <= 100; c++) {
 }
 console.log(soma39)
 
-// Ex 40 - Mostre os divisores de um número. 
-let numero40 = 12
-let soma40 = [] 
-for (let c = 1; c <= numero40; c++) {
-    if (numero40 % c === 0)
-        soma40.push(c)
-}
-console.log(soma40)
 
-// Ex 41 - Mostre todos os números primos entre 1 e 100.
 
 
 
