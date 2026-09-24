1f6a6de6845d1b211e33af0f947133579f84c956
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-23 21:47:16 -0300

    Update Ex002.js

--- Revisão/Ex048/Ex002.js
+++ Revisão/Ex048/Ex002.js
@@ -130,7 +130,13 @@ const ladoA25 = 40
 const ladoB25 = 15
 const ladoC25 = 40
 
-
+if (ladoA25 >= ladoB25 && ladoA25 >= ladoC25) {
+    if ((ladoB25 + ladoC25) > ladoA25) {
+        console.log('É capaz de formar um triângulo')
+    } else {
+        console.log('Não é capaz de formar um triângulo')
+    }
+}   
 
 // Ex 26 - Identifique se um triângulo é equilátero, isósceles ou escaleno.
 const triA26 = 20
